e386033fccfbec1feab1aab0dce1bd6bfbdb199e
Wenmin Zhang <wenmin.bao.zhang@gmail.com>
2024-04-08 22:29:38 -0400

    add v4
    Refactored code

--- README.md
+++ README.md
@@ -31,133 +31,46 @@ pip install -r requirements.txt
 
 To test the installation and display basic usages:
 ```
-python sharepro_loc.py -h
+python src/SharePro/sharepro_coloc.py -h
 ```
 
 ## Input files
 
-Example input files are included in the [dat](dat/) directory.
+Example input files are included in the [dat](dat/) directory. 
 
-### Run SharePro with a batch of zscore files and ld files
+1. The **GWAS files** contain four mandatory columns: SNP/BETA/SE/N. Here are examples for [BMD](dat/BMD_bse.txt) GWAS and [RSPO3](dat/RSPO3_bse.txt) pQTL zscore files.
 
-SharePro can take in a summary file as well as z-scores files and LD files as inputs.
-
-1. The **summary file** contains two mandatory columns: names of z-score files and ld files. Multiple files are allowed and should be separated by comma. An example can be found at [dat/BMD_RSPO3.zld](dat/BMD_RSPO3.zld).
-
-2. The **zscore files** contain two mandatory columns: variant IDs and z-scores. Here are examples for [BMD](dat/BMD_SH.txt) GWAS and [RSPO3](dat/RSPO3_SH.txt) pQTL zscore files.
-
-3. The **LD files** contain Pearson correlation between variants. **Please make sure the REF/ALT alleles used in calculating LD are the same as the GWAS study!!** An example can be found at [dat/RSPO3.ld](dat/RSPO3.ld) and a working script for matching raw GWAS summary statistics and PLINK bim file is [provided](match_bim_ss.py).
-
-### Run SharePro in one locus with GWAS (BETA/SE/N) and ld files
-
-SharePro can also use the matched GWAS summary statistics including effect size (BETA), standard error (SE) and sample size (N) and ld matrix from one locus. 
-
-1. The **GWAS file** contains three mandatory columns: BETA/SE/N. Here are examples for [BMD](dat/BMD_bse.txt) GWAS and [RSPO3](dat/RSPO3_bse.txt) pQTL zscore files.
-
-2. The **LD file** is the same as above.
-
+2. The **LD file** contains Pearson correlation between variants. **Please make sure the REF/ALT alleles used in calculating LD are the same as the GWAS study!!** An example can be found at [dat/RSPO3.ld](dat/RSPO3.ld).
 
 ## Usage examples
 
 We use the colocalization analysis of a RSPO3 cis-pQTL and eBMD GWAS as an example. The [dat/](dat/) folder contains all files required for this example.
 If you want to learn more about this locus, please refer to the [analysis repo](https://github.com/zhwm/SharePro_coloc_analysis/tree/main/dat).
 
-### Run SharePro with a batch of zscore files and ld files
-
-We use `--zld` to indicate the path to the summary file and `--zdir` to indicate the path to the zscore files.
-Additionally, we can specify the sample sizes of the eBMD GWAS study and RSPO3 pQTL study with `--N`.
-Next, we specify path to save results with `--save`. The max number of causal signals is set to 10 using `--K`.
+We use `--z` and `--ld` to indicate the path to the matched GWAS summary statistic files and the ld file, respectively. Additionally, we can specify path to save results with `--save`. The max number of causal signals can be set to 10 using `--K`.
 
 ```
-python sharepro_loc.py \
---zld dat/BMD_RSPO3.zld \
---zdir dat \
---N 426824 10708 \
---save res \
---verbose \
---K 10
-```
-
-### Run SharePro in one locus with GWAS (BETA/SE/N) and ld files
-
-Alternatively, we can use `--z` to provide path to the GWAS summary statistic files with three mandatory columns: BETA/SE/N. With `--ld`, we can provide path to the ld files. Lastly, we use `--save` to provide the filename for the colocalization result.
-
-```
-python sharepro_bse.py \
+python src/SharePro/sharepro_coloc.py \
 --z dat/BMD_bse.txt dat/RSPO3_bse.txt \
---ld dat/RSPO3.ld dat/RSPO3.ld \
---save res.txt \
---verbose \
+--ld dat/RSPO3.ld \
+--save doc/res \
 --K 10
 ```
 
 ## Output files
 
-Here are the expected outputs from the example above:
-
-```
-K = 10
-**********************************************************************
-Iteration-->0 . Likelihood: 1453.7 . KL_b: -25.1 . KL_c: -104.0 . KL_s: 40.6 . ELBO: 1365.2
-**********************************************************************
-Iteration-->1 . Likelihood: 1458.1 . KL_b: -23.0 . KL_c: -94.9 . KL_s: 39.2 . ELBO: 1379.5
-**********************************************************************
-Iteration-->2 . Likelihood: 1459.5 . KL_b: -22.4 . KL_c: -92.4 . KL_s: 39.1 . ELBO: 1383.8
-**********************************************************************
-Iteration-->3 . Likelihood: 1459.7 . KL_b: -22.4 . KL_c: -92.3 . KL_s: 39.1 . ELBO: 1384.1
-Colocalization probability: 1.0
-The 0-th effect group contains effective variants:
-causal variants: ['rs7741021', 'rs9482773']
-variant probabilities for this effect group: [0.5936, 0.4064]
-shared probability for this effect group: 1.0
-
-The 1-th effect group contains effective variants:
-causal variants: ['rs853974']
-variant probabilities for this effect group: [1.0]
-shared probability for this effect group: 1.0
-
-The 2-th effect group contains effective variants:
-causal variants: ['rs577721086', 'rs72959041']
-variant probabilities for this effect group: [0.5313, 0.4686]
-shared probability for this effect group: 0.0114
-
-The 3-th effect group contains effective variants:
-causal variants: ['rs717796', 'rs7775814', 'rs1512450', 'rs7775090', 'rs1512449', 'rs910536', 'rs6902741', 'rs1569870', 'rs7738255', 'rs1080708', 'rs7756072']
-variant probabilities for this effect group: [0.1215, 0.1166, 0.1016, 0.0878, 0.0866, 0.0849, 0.0784, 0.0746, 0.0705, 0.0704, 0.0638]
-shared probability for this effect group: 0.0001
-
-The 4-th effect group contains effective variants:
-causal variants: ['rs11759578', 'rs77525683', 'rs73593094', 'rs73593068', 'rs73577838', 'rs9482768']
-variant probabilities for this effect group: [0.6213, 0.2032, 0.0666, 0.031, 0.0264, 0.0131]
-shared probability for this effect group: 0.0003
-
-The 5-th effect group contains effective variants:
-causal variants: ['rs2800728', 'rs727330', 'rs727331', 'rs2800720', 'rs2800721', 'rs10456964', 'rs2800733', 'rs2800727', 'rs2745351', 'rs2745356', 'rs2800732', 'rs2745355', 'rs727332', 'rs2800729', 'rs2800730', 'rs719728', 'rs2800719', 'rs2745354', 'rs2800718', 'rs2800723', 'rs2800722']
-variant probabilities for this effect group: [0.0687, 0.0664, 0.0658, 0.0646, 0.0608, 0.0532, 0.0521, 0.0514, 0.0464, 0.0411, 0.041, 0.0404, 0.0398, 0.0395, 0.0387, 0.038, 0.034, 0.0328, 0.0324, 0.0323, 0.0322]
-shared probability for this effect group: 0.0002
-```
-
-The results have been saved into the **colocalization summary** (cs) files with columns:
+The results have been saved into the [**colocalization summary** file](doc/res.sharepro.txt) with columns:
 `cs` for variant representations in effect groups; 
 `share` for colocalization probabilities;
 `variantProb` for variant representation weights in effect groups;
-`k` for the max number of causal signals specified in the model.
 
-```
-$> cat res/BMD_SH.txt_RSPO3_SH.txt.cs 
-cs      share   variantProb     k
-rs7741021/rs9482773     1.0     0.5936/0.4064   10
-rs853974        1.0     1.0     10
-rs577721086/rs72959041  0.0114  0.5313/0.4686   10
-rs717796/rs7775814/rs1512450/rs7775090/rs1512449/rs910536/rs6902741/rs1569870/rs7738255/rs1080708/rs7756072     0.0001  0.1215/0.1166/0.1016/0.0878/0.0866/0.0849/0.0784/0.0746/0.0705/0.0704/0.0638      10
-rs11759578/rs77525683/rs73593094/rs73593068/rs73577838/rs9482768        0.0003  0.6213/0.2032/0.0666/0.031/0.0264/0.0131        10
-rs2800728/rs727330/rs727331/rs2800720/rs2800721/rs10456964/rs2800733/rs2800727/rs2745351/rs2745356/rs2800732/rs2745355/rs727332/rs2800729/rs2800730/rs719728/rs2800719/rs2745354/rs2800718/rs2800723/rs2800722    0.0002  0.0687/0.0664/0.0658/0.0646/0.0608/0.0532/0.0521/0.0514/0.0464/0.0411/0.041/0.0404/0.0398/0.0395/0.0387/0.038/0.034/0.0328/0.0324/0.0323/0.0322   10
-```
+The [log file](doc/res.sharepro.log) contains the expected output of the algorithm, including parameters and the ELBO in each iteration.
 
 ## Versions
 * Version 1.0.0 (2023/01/25) Initial release
 * Version 2.0.0 (2023/08/25) Added K=1 in case of LD mismatch
 * Version 3.0.0 (2024/02/21) Added scripts to run SharePro in one locus with GWAS (BETA/SE/N) and ld files
+* Version 4.0.0 (2024/04/08) Refactored code
 
 ## Citations
 
---
+++ src/SharePro/sharepro_coloc.py
@@ -0,0 +1,195 @@
+import argparse
+import logging
+import pandas as pd
+import numpy as np
+from scipy.special import softmax, expit
+
+np.set_printoptions(precision=4, linewidth=200)
+
+def title():
+    print('**********************************************************************')
+    print('* SharePro for accurate and efficient colocalization                 *')
+    print('* Version 4.0.0                                                      *')
+    print('* (C) Wenmin Zhang (wenmin.zhang@mail.mcgill.ca)                     *')
+    print('**********************************************************************')
+
+
+class SparseReg(object):
+    def __init__(self, P, K, N, sigma, varb):
+        """initialize and set parameters"""
+        self.p = P
+        self.k = K
+        self.n = N
+        self.sigma = sigma
+        self.lamb = np.array([k+1 for k in range(self.k)]) / self.n / varb / (1 - varb)
+        self.beta_mu = np.zeros([self.p, self.k])
+        self.delta = np.zeros((self.p, self.k))
+        self.y_tau = 1.0 / (1 - varb)
+        self.u1 = np.log(self.sigma / (1 - self.sigma)) + 0.5 * np.log(self.lamb / (1 + self.lamb))
+        self.priorb = np.array([k+1 for k in range(self.k)]) / varb
+        self.postn = self.n * self.y_tau + self.priorb
+
+    def infer_q_beta(self, b_hat_s, R_s, GAM, k):
+        """perform variational updates for the k-th effect with beta and c"""
+        idxall = list(range(self.k))
+        idxall.remove(k)
+        beta_all_k = (GAM[:, idxall] * self.beta_mu[:, idxall] * self.delta[:, idxall]).sum(axis=1)
+        res_beta = b_hat_s - np.dot(R_s, beta_all_k)
+        self.beta_mu[:, k] = 1 / (self.lamb[k] + 1) * res_beta
+        u = self.u1[k] + 0.5 * self.beta_mu[:, k] ** 2 * self.postn[k]
+        self.delta[:, k] = expit(u)
+        return u
+
+    def get_elbo(self, b_hat_s, R_s, GAM):
+        """get elbo with beta and c"""
+        beta_all = (GAM * self.delta * self.beta_mu).sum(axis=1)
+        ll1 = self.y_tau * np.dot(beta_all, self.n * b_hat_s)
+        ll2 = - 0.5 * self.y_tau * (
+            (((GAM * self.delta * (self.beta_mu ** 2 + 1 / self.postn)).sum(axis=1) * self.n).sum()))
+        W = GAM * self.delta * self.beta_mu
+        WtRW = np.dot(np.dot(W.transpose(), self.n * R_s), W)
+        ll3 = - 0.5 * self.y_tau * (WtRW.sum() - np.diag(WtRW).sum())
+        ll = ll1 + ll2 + ll3
+        mklbeta = - 0.5 * (GAM * self.delta * (
+                self.priorb * (self.beta_mu ** 2 + 1 / self.postn) +
+                np.log(self.postn / self.priorb) - 1)).sum()
+        nozerodelta = self.delta[self.delta != 0]
+        nozeroGAM = GAM[self.delta != 0]
+        noonedelta = self.delta[self.delta != 1]
+        nooneGAM = GAM[self.delta != 1]
+        mkldelta = (nozeroGAM * nozerodelta * np.log(self.sigma / nozerodelta)).sum() - (
+                nooneGAM * (1 - noonedelta) * np.log((1 - self.sigma) / (1 - noonedelta))).sum()
+        return ll, mklbeta, mkldelta
+
+
+class SharePro(object):
+    def __init__(self, P, K, num, sigma, Nlst, varblst):
+        """initialize and set parameters"""
+        self.p = P
+        self.k = K
+        self.num = num
+        self.sigma = sigma
+        self.prior_pi = np.ones((self.p,)) * (1 / self.p)
+        self.gamma = np.zeros((self.p, self.k))
+        self.usum1 = self.num * np.log(1 - self.sigma**(1/num)) + np.log(self.prior_pi)
+        self.SR = [SparseReg(P, K, Nlst[i], self.sigma**(1/num), varblst[i]) for i in range(self.num)]
+
+    def infer_q_s(self, bhatlst, ld):
+        """perform variational updates for s"""
+        for k in range(self.k):
+            idxall = list(range(self.k))
+            idxall.remove(k)
+            unum = np.array([j.infer_q_beta(bhatlst[i], ld, self.gamma, k) for (i,j) in enumerate(self.SR)])
+            unumexp = np.log(1 + np.exp(-unum))
+            uall = unum.sum(axis=0) + unumexp.sum(axis=0) + self.usum1
+            self.gamma[:, k] = softmax(uall)
+    
+    def get_effect(self, ld, cthres, pthres):
+        vidx = np.argsort(-self.gamma, axis=1)
+        matidx = np.argsort(-self.gamma, axis=0)
+        mat_eff = np.zeros((self.p, self.k))
+        for p in range(self.p):
+            mat_eff[p, vidx[p, 0]] = self.gamma[p, vidx[p, 0]]
+        matdelta = np.prod(np.array([i.delta for i in self.SR]), axis=0)
+        csum = mat_eff.sum(axis=0).round(2)
+        logging.info("Attainable coverage for effect groups: {}".format(csum))
+        eff = {}
+        eff_gamma = {}
+        eff_share = {}
+        for k in range(self.k):
+            if csum[k] > cthres:
+                p = 0
+                while np.sum(mat_eff[matidx[0:p, k], k]) < cthres * csum[k]:
+                    p = p + 1
+                cidx = matidx[0:p, k].tolist()
+                purity = abs(ld[np.ix_(cidx, cidx)]).min()
+                if purity > pthres:
+                    eff[k] = cidx
+                    eff_gamma[k] = mat_eff[eff[k], k].round(4).tolist()
+                    effgamma_n = [i/sum(eff_gamma[k]) for i in eff_gamma[k]]
+                    eff_share[k] = np.dot(matdelta[eff[k], k], effgamma_n).round(4)
+        return eff, eff_gamma, eff_share
+
+    def get_elbo(self, bhatlst, ld):
+        """get elbo with s"""
+        llsum, mklbetasum, mkldeltasum = zip(*[j.get_elbo(bhatlst[i], ld, self.gamma) for (i,j) in enumerate(self.SR)])
+        gammaterm1 = (self.gamma * np.tile(self.prior_pi.reshape(-1, 1), (1, self.k))).sum()
+        gammaterm2 = (self.gamma[self.gamma != 0] * np.log(self.gamma[self.gamma != 0])).sum()
+        mklgamma = gammaterm1 - gammaterm2
+        ll = sum(llsum)
+        mklbeta = sum(mklbetasum)
+        mkldelta = sum(mkldeltasum)
+        elbo = ll + mklbeta + mkldelta + mklgamma
+        return ll, mklbeta, mkldelta, mklgamma, elbo
+
+    def train(self, bhatlst, ld, maxite, eps, ubound):
+        loss = 100000.0
+        for ite in range(maxite):
+            self.infer_q_s(bhatlst, ld)
+            ll, mklbeta, mkldelta, mklgamma, elbo = self.get_elbo(bhatlst, ld)
+            logging.info('*' * 70)
+            logging.info('Iteration-->{} . Likelihood: {:.1f} . KL_b: {:.1f} . KL_c: {:.1f} . KL_s: {:.1f} . ELBO: {:.1f}'.format(ite, ll, mklbeta, mkldelta, mklgamma, elbo))
+            if abs(elbo - loss) < eps:
+                converged = True
+                break
+            if ite == (maxite - 1) or elbo > ubound:
+                print("Detected mismatch between summary statistics and LD matrix. Set K to 1...")
+                converged = False
+                break
+            loss = elbo
+        return converged
+
+def get_bhat(beta, se, n):
+    z = beta / se
+    bhat = z / np.sqrt(z**2 + n)
+    return bhat
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='SharePro Commands:')
+    parser.add_argument('--z', type=str, default=None, nargs='+', help='file to matched summary statistics', required=True)
+    parser.add_argument('--ld', type=str, default=None, help='path to matched ld matrix', required=True)
+    parser.add_argument('--save', type=str, default=None, help='path to save results', required=True)
+    parser.add_argument('--sigma', type=float, default=1e-5, help='prior colocalization prob')
+    parser.add_argument('--K', type=int, default=10, help='largest number of causal signals')
+    parser.add_argument('--maxite', type=int, default=100, help='max number of iterations')
+    parser.add_argument('--eps', type=float, default=0.1, help='convergence criterion')
+    parser.add_argument('--ubound', type=int, default=1000000, help='upper bound for inconvergence')
+    parser.add_argument('--cthres', type=float, default=0.95, help='attainable coverage threshold for effect groups')
+    parser.add_argument('--pthres', type=float, default=0.5, help='purity threshold for effect groups')
+    args = parser.parse_args()
+    return args
+
+def print_args(args):
+    for arg in vars(args):
+        logging.info(f"{arg}: {getattr(args, arg)}")
+
+def main(args):
+    zfile = [pd.read_csv(i, sep='\t') for i in args.z]
+    ld = pd.read_csv(args.ld, sep='\s+', header=None).values
+    bhatlst = [get_bhat(i['BETA'].values, i['SE'].values, i['N'].values) for i in zfile]
+    Nlst = [max(i['N']) for i in zfile]
+    varb = [(i**2).max() for i in bhatlst] 
+    model = SharePro(zfile[0].shape[0], args.K, len(zfile), args.sigma, Nlst, varb)
+    mc = model.train(bhatlst, ld, args.maxite, args.eps, args.ubound)
+    if mc:
+        eff, eff_gamma, eff_share = model.get_effect(ld, args.cthres, args.pthres)
+    else:
+        model = SharePro(zfile[0].shape[0], 1, len(zfile), args.sigma, Nlst, varb)
+        eff, eff_gamma, eff_share = model.get_effect(ld, args.cthres, args.pthres)
+    for e in eff:
+        mcs_idx = [zfile[0]['SNP'][j] for j in eff[e]]
+        logging.info(f'The {e}-th effect group contains effective variants:')
+        logging.info(f'causal variants: {mcs_idx}')
+        logging.info(f'variant probabilities for this effect group: {eff_gamma[e]}')
+        logging.info(f'shared probability for this effect group: {eff_share[e]}\n')
+    allcs = pd.DataFrame()
+    allcs['cs'] = ['/'.join(zfile[0]['SNP'][j]) for j in eff.values()]
+    allcs['share'] = ['{:.4}'.format(j) for j in eff_share.values()]
+    allcs['variantProb'] = ['/'.join([str(j) for j in i]) for i in eff_gamma.values()]
+    allcs.to_csv('{}.sharepro.txt'.format(args.save), sep='\t', header=True, index=False)    
+
+if __name__ == '__main__':
+    args = parse_args()
+    logging.basicConfig(filename='{}.sharepro.log'.format(args.save), level=logging.INFO)
+    print_args(args)
+    main(args)
